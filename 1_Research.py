d4242fc429c3a360544333881043c054eaee36fb
zackbelloula <z.belloula@univ-batna2.dz>
2025-06-26 18:45:13 +0100

    Delete 1_Research.py

--- 1_Research.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("📚 Research Projects")
-st.markdown("### 1. Predictive Maintenance using LSTM")
-st.write("...")
-st.markdown("### 2. Climate Data Analysis")
-st.write("...")
-st.markdown("### 3. NLP on Legal Texts")
-st.write("...")

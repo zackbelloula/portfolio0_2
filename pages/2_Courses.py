ad6343698863b243872e240505de281d8160e877
zackbelloula <z.belloula@univ-batna2.dz>
2025-06-26 18:43:56 +0100

    Add files via upload

---
+++ pages/2_Courses.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("🎓 Courses I Taught or Created")
+st.markdown("### 🧮 Introduction to Data Science")
+st.write("...")
+st.markdown("### 📊 Machine Learning 101")
+st.write("...")
+st.markdown("### 🔬 Deep Learning Basics")
+st.write("...")

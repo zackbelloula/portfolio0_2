c7f66218bad067e6e4589a27dbb87e2f446e8e35
zackbelloula <z.belloula@univ-batna2.dz>
2025-06-26 18:45:25 +0100

    Delete 2_Courses.py

--- 2_Courses.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("🎓 Courses I Taught or Created")
-st.markdown("### 🧮 Introduction to Data Science")
-st.write("...")
-st.markdown("### 📊 Machine Learning 101")
-st.write("...")
-st.markdown("### 🔬 Deep Learning Basics")
-st.write("...")
